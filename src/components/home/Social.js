478af4da4a8560f365ec2f833e70d4712a51597e
solcabz <41091153+solcabz@users.noreply.github.com>
2024-02-14 16:39:01 +0800

    social

--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Social() {
   return (
       <div className='home_social'>
-        <a href="https://www.instagram.com/" className="home_social-icon" target="_blank" rel="noreferrer"><i className="uil uil-airplay"></i></a>
+        <a href="https://www.linkedin.com/in/solomon-cabreza-91537219b/" className="home_social-icon" target="_blank" rel="noreferrer"><i className="uil uil-linkedin"></i></a>
         <a href="https://github.com/solcabz" className="home_social-icon" target="_blank" rel="noreferrer"><i className="uil uil-github"></i></a>
         <a href="https://codepen.io/solcabz" className="home_social-icon" target="_blank" rel="noreferrer"><i className='bx bxl-codepen'></i></a>
     </div>
