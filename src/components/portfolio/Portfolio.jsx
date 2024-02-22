f82e5a0c9d3aab0f30b73319b691b4c16f4eac46
solcabz <xhzoul30@gmail.com>
2024-02-22 09:12:22 +0800

    new added portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,17 +5,19 @@ const Portfolio = () => {
   return (
     <section className="section portfolio" id="portfolio">
       <div className="card-wrapper">
-        <a href="https://tic-tac-toe-sol.netlify.app/">
-          <img
-            src={projImage}
-            alt=""
-            style={{
-              width: "250px",
-              borderRadius: "8px",
-            }}
-          ></img>
-        </a>
-        <span>Tic-Tac-Toe</span>
+        <div>
+          <a href="https://tic-tac-toe-sol.netlify.app/">
+            <img
+              src={projImage}
+              alt=""
+              style={{
+                width: "250px",
+                borderRadius: "8px",
+              }}
+            ></img>
+          </a>
+          <span>Tic-Tac-Toe</span>
+        </div>
       </div>
     </section>
   );
