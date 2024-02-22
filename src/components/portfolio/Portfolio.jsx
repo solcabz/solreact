d525f95a614528834b6e5d101546756952ddd861
solcabz <41091153+solcabz@users.noreply.github.com>
2024-02-22 19:03:11 +0800

    portfolio update leo and design

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,25 +1,42 @@
 import projImage from "../../assets/tictac.png";
+import projLeo from "../../assets/leo.png";
 import "./portfolio.css";
 
 const Portfolio = () => {
   return (
-    <section className="section portfolio" id="portfolio">
+    <section className="section portfolio">
       <div className="card-wrapper">
-        <div className="image-box">
-          <a href="https://tic-tac-toe-sol.netlify.app/">
-            <img
-              src={projImage}
-              alt=""
-              style={{
-                width: "250px",
-                borderRadius: "8px",
-              }}
-            ></img>
-          </a>
-          <span>Tic-Tac-Toe</span>
-        </div>
+        <ProjectCard
+          image={projImage}
+          link="https://tic-tac-toe-sol.netlify.app/"
+          title="Tic-Tac-Toe"
+        />
+
+        <ProjectCard
+          image={projLeo}
+          link="https://www.leocasts.ph/"
+          title="LeocastPH Portfolio"
+        />
       </div>
     </section>
+  );
+};
+
+const ProjectCard = ({ image, link, title }) => {
+  return (
+    <div className="image-box">
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <img
+          src={image}
+          alt={`Screenshot of ${title} project`}
+          style={{
+            width: "250px",
+            borderRadius: "8px",
+          }}
+        />
+      </a>
+      <span>{title}</span>
+    </div>
   );
 };
 
